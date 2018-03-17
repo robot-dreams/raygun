f62c1d45811be3c6426a681c624d0744e8aa6b41
Elliott Jin <eyj@dropbox.com>
2018-03-17 12:25:22 -0700

    Add constants for 0 and 1 vectors

--- constants.go
+++ constants.go
@@ -3,7 +3,10 @@ package main
 import "github.com/go-gl/mathgl/mgl32"
 
 var (
-	black = mgl32.Vec3{0, 0, 0}
-	white = mgl32.Vec3{1, 1, 1}
+	zero3 = mgl32.Vec3{0, 0, 0}
+	ones3 = mgl32.Vec3{1, 1, 1}
+
+	black = zero3
+	white = ones3
 	blue  = mgl32.Vec3{0.5, 0.7, 1}
 )
