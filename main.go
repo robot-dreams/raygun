21fdfd04ef9d3a6d10ffb8010eaf1241aeaeb39a
Elliott Jin <eyj@dropbox.com>
2018-03-16 21:27:53 -0700

    Hello, world of graphics!

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nx := 200
+	ny := 100
+	fmt.Printf("P3\n%d %d\n255\n", nx, ny)
+	for j := ny - 1; j >= 0; j-- {
+		for i := 0; i < nx; i++ {
+			r := float32(i) / float32(nx)
+			g := float32(j) / float32(ny)
+			b := float32(0.2)
+			ir := int(255.99 * r)
+			ig := int(255.99 * g)
+			ib := int(255.99 * b)
+			fmt.Printf("%d %d %d\n", ir, ig, ib)
+		}
+	}
+}
